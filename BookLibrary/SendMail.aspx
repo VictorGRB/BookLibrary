f621ad02f34b7ac5f272f7348c81109307436780
VictorGRB <victorgarbo@yahoo.com>
2019-09-11 19:08:18 +0300

    commit a bit of viewgrid

--- BookLibrary/SendMail.aspx
+++ BookLibrary/SendMail.aspx
@@ -9,6 +9,20 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:GridView ID="gvBookLibrary" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:Boundfield DataField="Name" HeaderText="Full Name" />
+                    <asp:Boundfield DataField="EmailAddress" HeaderText="Email" />
+                    <asp:Boundfield DataField="Address" HeaderText="Address" />
+                    <asp:Boundfield DataField="PhoneNumber" HeaderText="Phone Number" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkSelect" Text="Send as Email" runat="server"></asp:LinkButton>
+                        </ItemTemplate>
+
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
         </div>
     </form>
 </body>
--- BookLibrary/SendMail.aspx.cs
+++ BookLibrary/SendMail.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace BookLibrary
 {
@@ -13,7 +14,15 @@ namespace BookLibrary
         string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Victor\source\repos\BookLibrary\BookLibrary\App_Data\Books.mdf;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Customer", sqlCon);
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+                gvBookLibrary.DataSource = dtbl;
+                gvBookLibrary.DataBind();
+            }
         }
     }
 }
