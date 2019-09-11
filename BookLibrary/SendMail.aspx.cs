cca9c1768d05f0a168726a1ca1ce5e1c047f5726
VictorGRB <victorgarbo@yahoo.com>
2019-09-11 15:06:10 +0300

    commit

---
+++ BookLibrary/SendMail.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace BookLibrary
+{
+    public partial class SendMail : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Victor\source\repos\BookLibrary\BookLibrary\App_Data\Books.mdf;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
